bff08f723345a70e0c140103f72d590bb8bdc546
nayaknishant656 <nayaknishant656@gmail.com>
2026-02-03 23:42:19 +0530

    developing the resources list in the webapp now

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Header from '@/components/header/header'
 import React from 'react'
 import Resources from '@/components/feature/resoruces/resourcemain'
 import './page.css'
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className="grand-parent-container-main">
       <div className="parent-container-main">
         <Header />
         <Resources />
+        <Link href="/about">About</Link>
       </div>
     </div>
   );
--- components/feature/resoruces/components/navigation.jsx
+++ components/feature/resoruces/components/navigation.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import './navigation.css'
 
 export default function Navigation() {
   return (
-    <div className="border border-red-500 p-[5px]">
-      <h2>Navigation Component</h2>
+    <div className="navigation_resources_grandparent">
+      <div className="navigation_resources_parent">
+        <div className="navigation_resources_parent_body">
+          <Link href="/navigation">FET</Link>
+          <Link href="/">FCSE</Link>
+          <Link href="/">FPA</Link>
+          <Link href="/">FDA</Link>
+          <Link href="/">FMC</Link>
+          <Link href="/">FSH</Link>
+          <Link href="/">FPH</Link>
+          <Link href="/">PIHM</Link>
+          <Link href="/">ASRC</Link>
+        </div>
+      </div>
     </div>
   )
 }
