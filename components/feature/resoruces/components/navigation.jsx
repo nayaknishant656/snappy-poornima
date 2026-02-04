f52e751f8c4f146071d7c56a9484e22113027f9c
nayaknishant656 <nayaknishant656@gmail.com>
2026-02-04 22:57:09 +0530

    navigation fix the LINK problem

--- components/feature/resoruces/components/navigation.jsx
+++ components/feature/resoruces/components/navigation.jsx
@@ -7,16 +7,16 @@ export default function Navigation() {
     <div className="navigation_resources_grandparent">
       <div className="navigation_resources_parent">
         <div className="navigation_resources_parent_body">
-          <Link href="/?faculty=FCE">FCE</Link>
-          <Link href="/?faculty=FET">FET</Link>
-          <Link href="/?faculty=FCSE">FCSE</Link>
-          <Link href="/?faculty=FPA">FPA</Link>
-          <Link href="/?faculty=FDA">FDA</Link>
-          <Link href="/?faculty=FMC">FMC</Link>
-          <Link href="/?faculty=FSH">FSH</Link>
-          <Link href="/?faculty=FPH">FPH</Link>
-          <Link href="/?faculty=PIHM">PIHM</Link>
-          <Link href="/?faculty=ASRC">ASRC</Link>
+          <Link href="/resources?faculty=FCE">FCE</Link>
+          <Link href="/resources?faculty=FET">FET</Link>
+          <Link href="/resources?faculty=FCSE">FCSE</Link>
+          <Link href="/resources?faculty=FPA">FPA</Link>
+          <Link href="/resources?faculty=FDA">FDA</Link>
+          <Link href="/resources?faculty=FMC">FMC</Link>
+          <Link href="/resources?faculty=FSH">FSH</Link>
+          <Link href="/resources?faculty=FPH">FPH</Link>
+          <Link href="/resources?faculty=PIHM">PIHM</Link>
+          <Link href="/resources?faculty=ASRC">ASRC</Link>
         </div>
       </div>
     </div>
