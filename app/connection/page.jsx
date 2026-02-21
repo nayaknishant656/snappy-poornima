4f6c069fad1971b376788d788019a52c2d290594
nayaknishant656 <nayaknishant656@gmail.com>
2026-02-21 17:33:20 +0530

    structure layout

---
+++ app/connection/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Connection from '../../components/feature/connection/connection'
+export default function page() {
+    return (
+        <Connection />
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <Header/>
-        {children}
+        <Header />
+        <div className="grand-parent-container-main">
+          <div className="parent-container-main"> {children} </div>
+        </div>
       </body>
     </html>
   );
