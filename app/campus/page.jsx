07d04f54f554cbb9ae13f7dfb69fafe61d973046
nayaknishant656 <nayaknishant656@gmail.com>
2026-03-04 00:38:41 +0530

    feat: Implement Campus page with header navigation, and refactor resource faculty names and styling.

---
+++ app/campus/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Campus from '../../components/feature/campus/campus'
+export default function page() {
+    return (
+        <Campus />
+    )
+}
---
+++ components/feature/campus/campus.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './campus.css'
+
+export default function campus() {
+    return (
+        <div className='grandparent_campus'>
+            <div className='parent_campus'>
+                <div className='grandpparent_campus_content'>
+                    <div className='parent_campus_content'>
+                        <div className='campus_header'>
+                            <h1>campus</h1>
+                        </div>
+                        <div className='campus_content_parent'>
+                            <h2>Poornima University Campus</h2>
+                            <h2>Poornima Institute of Enginerrning and Technology</h2>
+                            <h2>Poornima College of Engineering</h2>
+                            <h2>Jecrc,jaipur</h2>
+                            <h2>SKit,jaipur</h2>
+                            <h2>Vgu,jaipur</h2>
+                            <h2>Jaipur Naitional University </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
