c990089d70fa34ff97ae24def8a966bb9fd640b8
nayaknishant656 <nayaknishant656@gmail.com>
2026-03-21 22:30:11 +0530

    feat: Add Auditorium and Confession pages, implement a new Searchbot component, and refactor the campus catalog to dynamically render content.

---
+++ app/Auditorium/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='grandparent_auditorium'>
+            <div className='parent_auditorium'>
+                <h2>Poornima Univerisity - Auditorium Hall-2</h2>
+                <h4>Book your Seat Now </h4>
+            </div>
+        </div>
+    )
+}
