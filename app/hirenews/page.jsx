5aef26e7daa750d3af8c1f1fc20df5a62e71585c
nayaknishant656 <nayaknishant656@gmail.com>
2026-05-23 15:14:10 +0530

    feat: added a Feature in the Application about the Hirenews , hirenews regarding the Components application of the Hirenews

---
+++ app/hirenews/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>Hire News About Myself and Duration about the ProDuct and Project about the Details</div>
+    )
+}
